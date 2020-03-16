b9fb8770a5b228d367ded4af4b47e014c3b09d2c
github username <github email address>
2020-03-16 14:41:31 -0400

    committing final changes - all methods work

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,46 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    Gym.all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  def lifters
+    memberships.map do |membership|
+      membership.lifter
+    end
+  end
+
+  def lifter_names
+    lifters.map do |lifter|
+      lifter.name
+    end
+  end
+
+  def combined_lift
+    combined_totals = 0
+
+    lifters.map do |lifter|
+      combined_totals += lifter.lift_total
+    end
+
+    combined_totals
+  end
+
+
+
+
 end
