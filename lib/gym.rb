b5c60abcba85909546f7a67698e3575e11f5b10f
MisterC1ean <robert_keller44@icloud.com>
2020-03-16 14:17:54 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -12,4 +12,30 @@ class Gym
     @@all
   end
 
+  def memberships
+    Membership.all.select do |o|
+      o.gym == self
+    end
+  end
+  
+  def lifters
+      memberships.map do |o|
+        o.lifter
+      end
+  end
+
+  def lifter_names
+    lifters.map do |o|
+      o.name
+    end
+  end
+
+  def lift_total
+    total = 0
+    lifters.map do |lifter|
+     total += lifter.lift_total 
+    end
+    total
+  end
+
 end
