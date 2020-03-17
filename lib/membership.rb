309b854deb361e27da14cb5eb26efc217e970576
William <willlin1996@gmail.com>
2020-03-16 21:59:58 -0400

    Done.

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,14 @@
 class Membership
   attr_reader :cost
 
+  @@all 
+
   def initialize(cost)
     @cost = cost
+    Membership.all << self
   end
 end
+
+# **Membership**
+
+#   - Get a list of all memberships
